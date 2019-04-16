19546d4241692fe06a175f88bf0cd4d92599cf02
kabori257 <beyond_games4880@i.softbank.jp>
2019-04-17 00:46:33 +0900

    test

---
+++ Phase1/小課題1/line.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class line
+{
+
+    public point p, q;  //端点
+
+    public line(point p, point q){
+        this.p = p;
+        this.q = q;
+    }
+}
---
+++ Phase1/小課題1/point.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class point
+{
+    public float x, y;  //座標
+
+    public point(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+}

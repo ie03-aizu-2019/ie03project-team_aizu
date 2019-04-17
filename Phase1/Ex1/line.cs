ae1cb0c3ccf9613de404831147b5b8066d5fbffa
tealike <49443869+tealike@users.noreply.github.com>
2019-04-17 11:41:58 +0900

    lineの修正

--- Phase1/Ex1/line.cs
+++ Phase1/Ex1/line.cs
@@ -1,10 +1,10 @@
 using System;
 
 public class line{
-  public float x,y; //座標
+  public point p,q; //座標
 
-  public point(float x,float y){
-    this.x = x;
-    this.y = y;
+  public line(point p,point q){
+    this.p = p;
+    this.q = q;
   }
 }
